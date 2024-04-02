dbd482f387c03d701fddc400b0a7f93d8f6977c5
Blake <blake.perrin13@gmail.com>
2024-04-02 12:47:48 -0500

    Created script to connect to APs from command line

--- APSetup/Scripts/AccessPoint/ap_connect.py
+++ APSetup/Scripts/AccessPoint/ap_connect.py
@@ -1,67 +1,119 @@
-from APSetup.Scripts.AccessPoint.ap_scan import get_aps, clean_data, table_output
+from ap_scan import get_aps, clean_data, table_output
+import os
 import json
 
-filename = "TestData/AP_Scan/test_data.txt"
+directory = "../../Files/etc/wpa_supplicant/"
+ap_data_file = "../TestData/AP_Scan/test_data.txt"
 
 def connect(data):
-    with open(f"{data['SSID']}.conf", "w") as f:
-        if data['Authentication'] == "PSK":
-            password = input("Enter the password: ")
-
-            # read wpa_psk_template in as json
-            with open("../Files/etc/wpa_supplicant/wpa_psk_template.conf", "r") as template:
-                output = ""
-                for line in template:
-                    if "ssid" in line:
-                        output += f"\tssid=\"{data['SSID']}\"\n"
-                    elif "key_mgmt" in line:
-                        output += f"\tkey_mgmt=WPA-PSK\n"
-                    elif "psk" in line:
-                        output += f"\tpsk=\"{password}\"\n"
+    conf_file = directory + f"{data['SSID']}.conf"
+    with open(conf_file, "w") as f:
+        try:
+            if data['Authentication'] == "PSK":
+                while True:
+                    invalid_chars = ["\"", "\\", "\n", "\t", "]", "\'", "[", "{", "}", "|", "(", ")", ";", ":"]
+                    password = input("Enter the password: ")
+                    if len(password) < 1:
+                        print("Invalid password. Please try again.")
+                    elif any(char in password for char in invalid_chars):
+                        print("Invalid password. Please try again.")
                     else:
-                        output += line
-            
-                template.close()
+                        break
+
+                # read wpa_psk_template in as json
+                with open(directory + "wpa_psk_template.conf", "r") as template:
+                    output = ""
+                    for line in template:
+                        if "ssid" in line:
+                            output += f"\tssid=\"{data['SSID']}\"\n"
+                        elif "key_mgmt" in line:
+                            output += f"\tkey_mgmt=WPA-PSK\n"
+                        elif "psk" in line:
+                            output += f"\tpsk=\"{password}\"\n"
+                        else:
+                            output += line
                 
-            f.write(output)
+                    template.close()
+                    
+                f.write(output)
 
-        elif data['Authentication'] == "802.1x":
-
-            username = input("Enter the username: ")
-            password = input("Enter the password: ")
-
-            # read wpa_eap_template in as json
-            with open("../Files/etc/wpa_supplicant/wpa_enterprise_template.conf", "r") as template:
-                output = ""
-                for line in template:
-                    if "ssid=\"" in line:
-                        output += f"\tssid=\"{data['SSID']}\"\n"
-                    elif "key_mgmt" in line:
-                        output += f"\tkey_mgmt=WPA-EAP\n"
-                    elif "identity" in line:
-                        output += f"\tidentity=\"{username}\"\n"
-                    elif "password" in line:
-                        output += f"\tpassword=\"{password}\"\n"
+            elif data['Authentication'] == "802.1x":
+                invalid_chars = ["\"", "\\", "\n", "\t", "]", "\'", "[", "{", "}", "|", "(", ")", ";", ":"]
+                while True:
+                    username = input("Enter the username: ")
+                    if len(username) < 1:
+                        print("Invalid username. Please try again.")
+                    elif any(char in username for char in invalid_chars):
+                        print("Invalid username. Please try again.")
                     else:
-                        output += line
+                        break
+                while True:
+                    password = input("Enter the password: ")
+                    if len(password) < 1:
+                        print("Invalid password. Please try again.")
+                    elif any(char in password for char in invalid_chars):
+                        print("Invalid password. Please try again.")
+                    else:
+                        break
 
-                template.close()
+                # read wpa_eap_template in as json
+                with open(directory + "wpa_enterprise_template.conf", "r") as template:
+                    output = ""
+                    for line in template:
+                        if "ssid=\"" in line:
+                            output += f"\tssid=\"{data['SSID']}\"\n"
+                        elif "key_mgmt" in line:
+                            output += f"\tkey_mgmt=WPA-EAP\n"
+                        elif "identity" in line:
+                            output += f"\tidentity=\"{username}\"\n"
+                        elif "password" in line:
+                            output += f"\tpassword=\"{password}\"\n"
+                        else:
+                            output += line
 
-            f.write(output)
+                    template.close()
 
-        f.close()
+                f.write(output)
+
+            else:
+                with open(directory + "open_network_template.conf", "r") as template:
+                    output = ""
+                    for line in template:
+                        if "ssid" in line:
+                            output += f"\tssid=\"{data['SSID']}\"\n"
+                        else:
+                            output += line
+
+                    template.close()
+
+                f.write(output)
+
+            f.close()
+        except KeyboardInterrupt:
+            f.close()
+            os.remove(conf_file)
+            print("\nExiting...")
+            exit()
     
-
-
-data = clean_data(get_aps(filename))
+data = clean_data(get_aps(ap_data_file))
 
 table_output(data)
 
-ap_num = int(input("\nChoose an Access Point to connect to: ")) - 1 
-key = list(data.keys())[ap_num]
-ap = data[key]
+while True:
+    try: 
+        ap_num = int(input("\nChoose an Access Point to connect to: ")) - 1
 
-print(f"\nConnecting to {ap['SSID']}")
+        if ap_num < 0:
+            print("Invalid Access Point")
+            continue
+        else:
+            key = list(data.keys())[ap_num]
+            ap = data[key]
+            print(f"\nConnecting to {ap['SSID']}")
+            connect(ap)
+            print(f"Connected to {ap['SSID']}")
+            exit()
 
-connect(ap)
+    except (IndexError, ValueError):
+        print("Invalid Access Point")
 
