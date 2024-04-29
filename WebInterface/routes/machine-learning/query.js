2773d0b30c61f3d6ecc3440e958e8a7d7f72161d
Blake <blake.perrin13@gmail.com>
2024-04-29 18:40:58 -0500

    ML data now displaying to the web page

--- WebInterface/routes/machine-learning/query.js
+++ WebInterface/routes/machine-learning/query.js
@@ -1,15 +1,25 @@
 const org = "PHOEBUS";
+const MLtoken = "0D5m1NEx3LGnX2NAZd2s64u6J7XOIuNDlz3K4bSwMUiIQS-NTmCeJcC_kLv6W2Alynn_7TkPvRTr3AftZadyMw==";
 
 function query(org, viewMalicious) {
 
-    var query_data = `
-    from(bucket: "${viewMalicious ? 'ML_malicious' : 'ML_benign'}")
+    if (viewMalicious == 1) {
+      var query_data = `from(bucket: "ML_malicious")
       |> range(start: -30m)
       |> filter(fn: (r) => r["_measurement"] == "session")
       |> filter(fn: (r) => r["_field"] == "flow_duration" or r["_field"] == "number_of_packets")
       |> pivot(rowKey: ["_time"], columnKey: ["_field"], valueColumn: "_value")
       |> group(columns:[])
       |> yield()`;
+    } else {
+      var query_data = `from(bucket: "ML_benign")
+      |> range(start: -30m)
+      |> filter(fn: (r) => r["_measurement"] == "session")
+      |> filter(fn: (r) => r["_field"] == "flow_duration" or r["_field"] == "number_of_packets")
+      |> pivot(rowKey: ["_time"], columnKey: ["_field"], valueColumn: "_value")
+      |> group(columns:[])
+      |> yield()`
+    }
 
     const options = {
         method: 'POST',
@@ -21,10 +31,11 @@ function query(org, viewMalicious) {
         }
     }
 
-    var data = fetch(`http://${location.host}:8086/api/v2/query?org=${org}`, options)
+    var data = fetch(`http://10.0.1.1:8086/api/v2/query?org=${org}`, options)
     .then(response => response.text())
     .then(text => {return Papa.parse(text, { header: true }) })
-    .then(val => console.log(val));
+    
+    data.then(val => console.log(val));
 
     return data;
   }
